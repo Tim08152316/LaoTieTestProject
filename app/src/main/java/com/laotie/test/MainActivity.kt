cbee8945789225f4cd5f3dd69f57811b4e28ec8c
Tim <huangtiebing@xverse.cn>
2022-07-21 14:15:02 +0800

    change MainActivity

--- app/src/main/java/com/laotie/test/MainActivity.kt
+++ app/src/main/java/com/laotie/test/MainActivity.kt
@@ -2,8 +2,11 @@ package com.laotie.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.laotie.test.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,13 +23,41 @@ class MainActivity : AppCompatActivity() {
         binding.sampleText.text = stringFromJNI()+"test"
         binding.sampleText.text = stringFromJNI()+"change on the remote main 001"
         binding.sampleText.text = stringFromJNI()+"change on the remote main 002"
-         binding.sampleText.text = stringFromJNI()+"change on the remote main 003"
+        binding.sampleText.text = stringFromJNI()+"change on the remote main 003"
         
         binding.sampleText.text = stringFromJNI()+"change on the local test branch ----01"
         binding.sampleText.text = stringFromJNI()+"change on the local test branch ----02"
 
         binding.sampleText.text = stringFromJNI()+"change on the local test branch ----03"
         binding.sampleText.text = stringFromJNI()+"change on the local test branch ----04"
+
+        GlobalScope.launch(context = Dispatchers.IO) {
+            //延时一秒
+            delay(1000)
+            Log.e("MainActivity","launch");
+        }
+        //主动休眠两秒，防止 JVM 过快退出
+        Log.e("MainActivity","start");
+        Thread.sleep(2000)
+        Log.e("MainActivity","end");
+
+//        val time = measureTimeMillis {
+        GlobalScope.launch  {
+                val asyncA = async {
+                    delay(3000)
+                    1
+                }
+                val asyncB = async {
+                    delay(4000)
+                    2
+                }
+                Log.e(MainActivity.javaClass.name,"msg:"+asyncA.await() + asyncB.await())
+            }
+//        }
+
+//        Thread.sleep(60000)
+
+        Log.e(MainActivity.javaClass.name, "time:")
     }
 
     fun testQuoteClick(view: View) {
@@ -36,6 +67,7 @@ class MainActivity : AppCompatActivity() {
     fun testWaitLockClick(view: View) {
         testWaitLock()
     }
+
 
     /**
      * A native method that is implemented by the 'Test' native library,
